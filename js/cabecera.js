8959ef79d11557ca74b18640523eef311236d43a
Jose Rojas Paredes <jrojasp1992@gmail.com>
2020-04-03 17:54:40 -0500

    cambio2

---
+++ js/cabecera.js
@@ -0,0 +1,24 @@
+Vue.component('cabecera', {
+    template: //html
+    `
+    <header>
+    <img src="img/WhatsApp Image 2020-04-02 at 9.50.48 PM.jpeg" alt="">
+    <div class="header">
+        <a href="index.html">Inicio</a>
+        <a href="">Servicios</a>
+        <a href="contacto.html">Alerta</a>
+    </div>
+    
+</header>
+    `
+});
+
+Vue.component('piedepagina', {
+    template: //html
+    `
+    <footer class="footer">
+    <h2>footer</h2>
+</footer>
+    `
+});
+
